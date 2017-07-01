c2e8e7877c96d6301da106fe9c0900eee00d9b8d
shifenghu <chinashifenghu@gmail.com>
2017-07-01 18:00:50 +0800

    修复了 pc 锚链接问题

--- angular/js/services/ohnet-utils.js
+++ angular/js/services/ohnet-utils.js
@@ -186,12 +186,12 @@ angular.module('ohnet').service('ohnetUtils',function ($q, OHNET_PROXY, $log, $t
     		return '';
     	}
     	// 如果元素存在，这里为了处理 动态 directive 生成的静态元素
-    	// if(!angular.isUndefined($element)){
-    	// 	$element.on('click', '[ui-scroll-to]', function(){
-    	// 		$location.hash($(this).attr('ui-scroll-to'));
-         //  		$anchorScroll();
-    	// 	});
-    	// }
+    	if(!angular.isUndefined($element) && !this.isMobileDevice()){
+    		$element.on('click', '[ui-scroll-to]', function(){
+    			$location.hash($(this).attr('ui-scroll-to'));
+          		$anchorScroll();
+    		});
+    	}
     	return '<a ui-scroll-to="comment-' + node._id + '-id" class="comment-icon"><span class="icon-info"></span></a>';
     };
 
